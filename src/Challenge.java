94cf720b4c5ac60f76661aa0f89e012e8d28c0b1
Jihed Berlim <jihedberlim@gmail.com>
2024-10-25 15:49:25 -0300

    feat: insert menu and Scanner options

--- src/Challenge.java
+++ src/Challenge.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 public class Challenge {
     public static void main(String[] args) {
 
         String name = "Jihed Elmograbi Berlim";
         String accountType = "Corrente";
         double currentBalance = 2500.00;
+        int choice = 0;
 
         System.out.println("**************************************");
         System.out.println("Dados iniciais do cliente:");
@@ -12,6 +15,18 @@ public class Challenge {
         System.out.println("Saldo inicial: R$ " + currentBalance);
         System.out.println("\n**************************************");
 
+        String menu = """
+                \nOperações
+                
+                1. Consultar saldo
+                2. Receber valores
+                3. Transferir valores
+                4. Sair
+                """;
+
+        Scanner typedValue = new Scanner(System.in);
+
 
     }
+
 }
