a42e64c1178e0aacd4938a54b9f20b29d17ac670
Jihed Berlim <jihedberlim@gmail.com>
2024-10-25 15:46:57 -0300

    Initial Commit

---
+++ src/Challenge.java
@@ -0,0 +1,17 @@
+public class Challenge {
+    public static void main(String[] args) {
+
+        String name = "Jihed Elmograbi Berlim";
+        String accountType = "Corrente";
+        double currentBalance = 2500.00;
+
+        System.out.println("**************************************");
+        System.out.println("Dados iniciais do cliente:");
+        System.out.println("\nNome do cliente: " + name);
+        System.out.println("Tipo da conta: " + accountType);
+        System.out.println("Saldo inicial: R$ " + currentBalance);
+        System.out.println("\n**************************************");
+
+
+    }
+}
