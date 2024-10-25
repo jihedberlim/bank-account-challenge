f4c24583caa482b0eb58f491dcea267297f1b80b
Jihed Berlim <jihedberlim@gmail.com>
2024-10-25 15:50:25 -0300

    feat: insert loop and conditionals for selecting options

--- src/Challenge.java
+++ src/Challenge.java
@@ -26,7 +26,32 @@ public class Challenge {
 
         Scanner typedValue = new Scanner(System.in);
 
+        while (choice != 4) {
+            System.out.println(menu);
+            choice = typedValue.nextInt();
 
+            if (choice == 1) {
+                System.out.println("\nSaldo atual: R$ " + currentBalance);
+            } else if (choice == 2) {
+                System.out.println("\nDigite o valor que irá receber: ");
+                double newValue = typedValue.nextDouble();
+                currentBalance += newValue;
+                System.out.println("\nNovo saldo: R$ " + currentBalance);
+            } else if (choice == 3) {
+                System.out.println("\nDigite o valor que deseja transferir: ");
+                double transfer = typedValue.nextDouble();
+                if (transfer > currentBalance) {
+                    System.out.println("\nSaldo insuficiente para realizar essa transferência! O seu saldo atual é: R$ " + currentBalance);
+                } else {
+                    currentBalance -= transfer;
+                    System.out.println("\nSaldo atual: R$ " + currentBalance);
+                }
+            } else if (choice == 4) {
+                System.out.println("\nSaindo da consulta!");
+            } else {
+                System.out.println("\nOpção inválida, escolha as opções disponíveis");
+            }
+        }
     }
 
 }
